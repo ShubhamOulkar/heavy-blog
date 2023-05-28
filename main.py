5b6c9e187672421b137898d1472e522fb19aee6d
shubham oulkar <oulkarshubhu@gmail.com>
2023-05-28 10:18:28 +0530

    Website updates

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ Bootstrap(app)
 
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')   # 'sqlite:///blogs.db'    
+app.config['SQLALCHEMY_DATABASE_URI'] =  os.environ.get('DATABASE_URL')   # 'sqlite:///blogs.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
